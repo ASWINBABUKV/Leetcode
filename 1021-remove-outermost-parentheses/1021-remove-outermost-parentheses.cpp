3bcfc7e10ba78255f2e66d3a8b759858ad82b582
ASWIN BABU K V <137376192+ASWINBABUKV@users.noreply.github.com>
2023-07-29 20:15:27 +0530

    Time: 4 ms (47.97%), Space: 6.6 MB (95.91%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        stack<char>st;
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                if(s[i]=='('){
+                    ans+=s[i];
+                    st.push(s[i]);
+                }
+                else{
+                    st.pop();
+                    if(!st.empty()){
+                        ans+=s[i];
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
